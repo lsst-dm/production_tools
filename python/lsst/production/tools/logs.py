82bf33fed01add5cd7f365e55995164e165f3e0a
Colin Slater <ctslater@uw.edu>
2022-01-04 19:33:45 -0800

    List matching log datasets.

--- python/lsst/production/tools/logs.py
+++ python/lsst/production/tools/logs.py
@@ -19,5 +19,30 @@ def collections():
 
     return jsonify(output)
 
+@bp.route("/dataId")
+def dataId():
+    butler = dafButler.Butler("/Users/ctslater/ci_imsim/DATA/butler.yaml")
+
+    collection = request.args.get("collection")
+
+    dimensions = ["visit", "detector", "tract", "patch"]
+    dataId = {"instrument": "LSSTCam-imSim"}
+    for dim in dimensions:
+        value = request.args.get(dim, type=int)
+        if(value is not None):
+            dataId[dim] = value
+
+    if(collection is None):
+        return "Must specify a collection"
+
+    dataId_string = ", ".join("{:s} {}".format(dim.capitalize(), val) for (dim, val) in dataId.items())
+
+    dataRefs = butler.registry.queryDatasets("*_log", dataId=dataId,
+                                             collections=collection)
+
+    log_types = [x.datasetType.name for x in dataRefs]
+
+    return render_template("logs/dataId.html", dataId=dataId_string,
+                          collection=collection, log_types=log_types)
 
 
